362b0f9cdfae701def188be5faa97cadf76836b6
Xuan Do <doxuancntp@gmail.com>
2019-05-29 12:15:57 -0400

    Commit.

--- simple-punchbag-game/js/app.js
+++ simple-punchbag-game/js/app.js
@@ -2,18 +2,39 @@ new Vue({
     el: '#vue-app',
     data: {
         health: 100,
-        ended: false
+        ended: false,
+        background: 'green'
     },
     methods: {
-        punch: function() {
+        punch: function(sound) {
             this.health -= 10;
             if (this.health <= 0) {
                 this.ended = true;
+            }
+            if (this.health <= 50) {
+                this.background = 'crimson';
+            }
+            if (this.health > 0) {
+                this.playSound(sound);
+            }
+            if (this.ended) {
+                this.alertRestart();
             }
         },
         restart: function() {
             this.health = 100;
             this.ended = false;
+            this.background = 'green';
+        },
+        playSound: function(sound) {
+            if (sound) {
+                let audio = new Audio(sound);
+                audio.play();
+            }
+        },
+        alertRestart: function() {
+            alert('You lose!');
+            this.restart();
         }
     },
     computed: {
